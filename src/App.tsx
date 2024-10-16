16fd493fa2634ab7094360a64e771cba7e09952b
Oleksandr Vlasov <drumlife182@gmail.com>
2024-10-16 20:22:37 +0200

    Working with react context

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import { AuthProvider } from "./domains/components/Context/Authentication/AuthProvider";
+import Login from "./domains/components/Context/Authentication/Login";
+import Navbar from "./domains/components/Context/Authentication/Navbar";
+import Context from "./domains/components/Context/Context/Context";
+import ContextMultiple from "./domains/components/Context/ContextMultiple/ContextMultiple";
 import ContextObject from "./domains/components/Context/ContextObject/ContextObject";
 import ThemeContextComponent from "./domains/components/Context/ContextValue/ThemeContext";
 
@@ -8,6 +13,23 @@ function App() {
 
       <div>
         <ContextObject />
+      </div>
+
+      <div>
+        <ContextMultiple />
+      </div>
+
+      <div>
+        <Context />
+      </div>
+
+      <div>
+        <AuthProvider>
+          <div>
+            <Navbar />
+            <Login />
+          </div>
+        </AuthProvider>
       </div>
     </div>
   );
